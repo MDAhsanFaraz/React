6f92f56b0446fd222c21207d4a9bf6a8d3fb65c0
Faraz <faraz.com>
2023-09-09 00:09:28 +0530

    second lecture

---
+++ 02_chitChat/index.js
@@ -0,0 +1,19 @@
+function x()
+{
+          const a=90;
+
+}
+x();
+
+var c=function ()
+{
+          console.log("I am an anonymous function");
+}
+c();
+
+var f=()=>{
+          console.log("dammm")
+}
+dsfg
+
+f(); 
